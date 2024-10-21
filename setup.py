69b527be007c12a2dac5e1829539d01a3d7ae469
Edgar Dorausch <edgar.dorausch@tu-dresden.de>
2024-10-21 17:22:02 +0200

    Added setuptool config

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup()
+
+# A minimal setup file, so develpment mode can be used with pip versions < 21.2 
+# (see https://setuptools.pypa.io/en/latest/userguide/quickstart.html#development-mode)
